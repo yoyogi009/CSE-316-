156b4f8ace292cbee972ec99751fb6f7f0d43f82
yoyogi009 <43295842+yoyogi009@users.noreply.github.com>
2019-04-15 22:41:23 +0530

    remove warnings

--- Q54OS.c
+++ Q54OS.c
@@ -1,57 +1,187 @@
 #include<stdio.h>
-int main()
-{
-	int p[20],bt[20], su[20], wt[20],tat[20],i, k, n, temp;
-	float wtavg, tatavg;
-	printf("Enter the number of PROCESS  in the queue --- ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		p[i] = i;
-		printf("Enter the Burst Time for process  %d --- ", i);
-		scanf("%d",&bt[i]);
-		printf("teacher/student process (0/1) ? --- ");
-		scanf("%d", &su[i]);
+#include<unistd.h>
+struct resource{
+
+	int pen;
+	int paper;
+	int q_paper;
+	int allot_id;
+
+};
+struct t_resource{
+
+	int pen_count ;
+	int paper_count ;
+	int q_paper_count ;
+};
+
+struct t_resource t[3];
+
+void allotment(struct resource a){
+	if(a.allot_id == 0){
+		a.pen=1;
 	}
-	
-	for(i=0;i<n;i++)
-	{
-		for(k=i+1;k<n;k++)
-		{
-			if(su[i] > su[k])
-			{
-				temp=p[i];
-				p[i]=p[k];
-				p[k]=temp;
-				
-				temp=bt[i];
-				bt[i]=bt[k];
-				bt[k]=temp;
-				
-				temp=su[i];
-				su[i]=su[k];
-				su[k]=temp;
+	else if(a.allot_id == 1){
+		a.paper=1;
+	}
+	else if(a.allot_id == 2){
+		a.q_paper=1;
+	}
+
+}
+
+
+int main(){
+	struct resource s[3];
+
+	int i,j,count=0;
+	for(i=0;i<3;i++){
+		s[i].allot_id=i;
+		allotment(s[i]);
+	}
+
+	printf("\nResources alloted are                          :");
+	for(i=0;i<3;i++){
+		if(s[i].pen == 1)
+		printf("\nResources alloted to student %d are        : pen ",i+1);
+
+		if(s[i].paper)
+		printf("\nResources alloted to student %d are        : paper ",i);
+
+		if(s[i].q_paper == 1)
+		printf("\nResources alloted to student %d are        : question paper ",i+1);
+	}
+
+	while(count != 3){
+		for(i=0;i<3;i++){
+			if(i=0){
+				t[i].paper_count=1;
+				t[i].pen_count=1;
+				for(j=0;j<3;j++){
+					if(s[j].q_paper==1){
+						printf("\nStudent %d has completed his job !",j+1);
+						count++;
+					}
+				}
+			}
+
+			if(i=1){
+				t[i].q_paper_count=1;
+				t[i].pen_count=1;
+				for(j=0;j<3;j++){
+					if(s[j].paper==1){
+						printf("\nStudent %d has completed his job !",j+1);
+						count++;
+					}
+				}
+			}
+
+			if(i=2){
+				t[i].paper_count=1;
+				t[i].q_paper_count=1;
+				for(j=0;j<3;j++){
+					if(s[j].pen==1){
+						printf("\nStudent %d has completed his job !",j+1);
+						count++;
+					}
+				}
 			}
 		}
-	}
-	wtavg = wt[0] = 0;
-	tatavg = tat[0] = bt[0];
-	for(i=1;i<n;i++)
-	{
-		wt[i] = wt[i-1] + bt[i-1];
-		tat[i] = tat[i-1] + bt[i];
-		wtavg = wtavg + wt[i];
-		tatavg = tatavg + tat[i];
-	}
-	printf("\nPROCESS\t\t TEACHER/STUDENT PROCESS  \tBURST TIME\tWAITING TIME\tTURNAROUND TIME");
-	
-	for(i=0;i<n;i++)
-	{
-		printf("\n%d \t\t %d \t\t %d \t\t %d \t\t\t %d ",p[i],su[i],bt[i],wt[i],tat[i]);
-	}
 
-	printf("\nAverage Waiting Time is --- %f",wtavg/n);
-	printf("\nAverage Turnaround Time is --- %f",tatavg/n);
-
+	}
 	return 0;
+
+}#include<stdio.h>
+#include<unistd.h>
+struct resource{
+
+	int pen;
+	int paper;
+	int q_paper;
+	int allot_id;
+
+};
+struct t_resource{
+
+	int pen_count ;
+	int paper_count ;
+	int q_paper_count ;
+};
+
+struct t_resource t[3];
+
+void allotment(struct resource a){
+	if(a.allot_id == 0){
+		a.pen=1;
+	}
+	else if(a.allot_id == 1){
+		a.paper=1;
+	}
+	else if(a.allot_id == 2){
+		a.q_paper=1;
+	}
+
+}
+
+
+int main(){
+	struct resource s[3];
+
+	int i,j,count=0;
+	for(i=0;i<3;i++){
+		s[i].allot_id=i;
+		allotment(s[i]);
+	}
+
+	printf("\nResources alloted are                          :");
+	for(i=0;i<3;i++){
+		if(s[i].pen == 1)
+		printf("\nResources alloted to student %d are        : pen ",i+1);
+
+		if(s[i].paper)
+		printf("\nResources alloted to student %d are        : paper ",i);
+
+		if(s[i].q_paper == 1)
+		printf("\nResources alloted to student %d are        : question paper ",i+1);
+	}
+
+	while(count != 3){
+		for(i=0;i<3;i++){
+			if(i=0){
+				t[i].paper_count=1;
+				t[i].pen_count=1;
+				for(j=0;j<3;j++){
+					if(s[j].q_paper==1){
+						printf("\nStudent %d has completed his job !",j+1);
+						count++;
+					}
+				}
+			}
+
+			if(i=1){
+				t[i].q_paper_count=1;
+				t[i].pen_count=1;
+				for(j=0;j<3;j++){
+					if(s[j].paper==1){
+						printf("\nStudent %d has completed his job !",j+1);
+						count++;
+					}
+				}
+			}
+
+			if(i=2){
+				t[i].paper_count=1;
+				t[i].q_paper_count=1;
+				for(j=0;j<3;j++){
+					if(s[j].pen==1){
+						printf("\nStudent %d has completed his job !",j+1);
+						count++;
+					}
+				}
+			}
+		}
+
+	}
+	return 0;
+
 }
